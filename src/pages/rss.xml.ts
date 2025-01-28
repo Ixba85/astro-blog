46c0f0a44ac33555a8528bb9a1e4893dcf0780d4
Ixba85 <jordicomasa@gmail.com>
2025-01-27 18:32:32 -0600

    enhance RSS feed generation with Markdown parsing and HTML sanitization

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,29 +2,49 @@ import rss from '@astrojs/rss';
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
 
-export const GET: APIRoute = async({ params, request, site }) => {
+import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
 
-    const blogPosts = await getCollection('blog');
+const parser = new MarkdownIt();
 
+export const GET: APIRoute = async ({ params, request, site }) => {
+  const blogPosts = await getCollection('blog');
 
-    return rss({
-        // stylesheet: '/styles/rss.xsl',
-        // `<title>` field in output xml
-        title: 'Jordi’s Blog',
-        // `<description>` field in output xml
-        description: 'Aprendiendo con Astro',
-        // Pull in your project "site" from the endpoint context
-        // https://docs.astro.build/en/reference/api-reference/#site
-        site: site ?? '',
-        // Array of `<item>`s in output xml
-        // See "Generating items" section for examples using content collections and glob imports
-        items: blogPosts.map((data, slug) => ({
-            title: data.data.title, 
-            pubDate: data.data.date,
-            description: data.data.description,
-            link: `/posts/${slug}`,
-        })),
-        // (optional) inject custom xml
-        customData: `<language>es-es</language>`,
-      });
-}
+  return rss({
+    // stylesheet: '/styles/rss.xsl',
+    // `<title>` field in output xml
+    title: 'Jordi Blog',
+    // `<description>` field in output xml
+    description: 'Un simple blog sobre mis aventura con Astro',
+
+    xmlns: {
+      media: 'http://search.yahoo.com/mrss/',
+    },
+
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#contextsite
+    site: site ?? '',
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: blogPosts.map(({ data, slug, body }) => ({
+      title: data.title,
+      pubDate: data.date,
+      description: data.description,
+      link: `/posts/${slug}`,
+
+      content: sanitizeHtml(parser.render(body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
+
+      customData: `<media:content
+          type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+          width="${data.image.width}"
+          height="${data.image.height}"
+          medium="image"
+          url="${site + data.image.src}" />
+      `,
+    })),
+    // (optional) inject custom xml
+    customData: `<language>es-mx</language>`,
+  });
+};
